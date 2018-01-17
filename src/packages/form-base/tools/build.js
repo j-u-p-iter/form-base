8dc0253ccca3e5f6f3d068652b32b7faca130c62
kirill <best.portfoleo@gmail.com>
2018-01-17 11:14:18 +0500

    Update build tool

--- src/packages/form-base/tools/build.js
+++ src/packages/form-base/tools/build.js
@@ -1,67 +1,41 @@
 const fs = require('fs');
 const spawn = require('child_process').spawn;
 const Duplex = require('stream').Duplex;
+const { forEachObjIndexed } = require('ramda');
+const chalk = require('chalk');
+
+const INFO_TO_COMPILE = require('./info-to-compile');
 
 
-const COMPILE_DATA = {
-  TO_ES: { 
-    command: 'npx babel modules -d es --ignore __tests__', 
-    moduleType: 'es',
-    messages: {
-      onBeforeRun: 'Compiling to ES modules\n', 
-      onSuccess: 'Compiled to ES modules with success\n',
-    },
-  },
-  TO_CMJS: { 
-    command: 'babel modules -d cmjs --ignore __tests__', 
-    moduleType: 'cmjs', 
-    messages: {
-      onBeforeRun: 'Compiling to CMJS modules', 
-      onSuccess: 'Compiled to CMJS modules with success',
-    },
-  },
-  TO_DEV_UMD: {
-    command: 'rollup -c -i modules/index.js -o umd/form-base.js',
-    moduleType: 'es',
-    env: 'development',
-    messages: {
-      onBeforeRun: 'Compiling to development UMD module',
-      onSuccess: 'Compiled to development UMD module with success',
-    },
-  },
-  TO_PROD_UMD: {
-    command: 'rollup -c -i modules/index.js -o umd/form-base.min.js',
-    moduleType: 'es',
-    env: 'production',
-    messages: {
-      onBeforeRun: 'Compiling to production UMD module',
-      onSuccess: 'Compiled to production UMD module with success',
-    },
-  },
-};
-
-const exec = (command, extraEnv) => 
+const exec = (command, extraEnv) =>
   spawn(command, {
     shell: true,
     env: { ...process.env, ...extraEnv },
   });
 
+const writeMessage = message => process.stdout.write(message);
+
 const createLogger = messages => (new Duplex({
   read() {
-    process.stdout.write(messages.onBeforeRun);
+    writeMessage(chalk.blue(messages.onBeforeRun));
     this.push(null);
   },
   write(chunk, encoding, callback) {
-    process.stdout.write(messages.onSuccess);
+    writeMessage(chalk.green(messages.onSuccess));
     callback();
   },
 }));
 
+const compileAccordingToInfo = ({
+  messages,
+  command,
+  moduleType,
+}) => {
+  const compileToESLogger = createLogger(messages);
+  const execCompilingToES = exec(command, { BABEL_ENV: moduleType });
 
-const compileDataScope = COMPILE_DATA.TO_ES;
-const compileToESLogger = createLogger(compileDataScope.messages); 
-const execCompilingToES = exec(compileDataScope.command, { BABEL_ENV: compileDataScope.moduleType });
+  compileToESLogger.pipe(execCompilingToES.stdin);
+  execCompilingToES.stdout.pipe(compileToESLogger);
+};
 
-compileToESLogger.pipe(execCompilingToES.stdin);
-execCompilingToES.stdout.pipe(compileToESLogger);
-
+forEachObjIndexed(compileAccordingToInfo, INFO_TO_COMPILE);
---
+++ src/packages/form-base/tools/info-to-compile.js
@@ -0,0 +1,39 @@
+const INFO_TO_COMPILE = {
+  TO_ES: {
+    command: 'npx babel modules -d es --ignore __tests__',
+    moduleType: 'es',
+    messages: {
+      onBeforeRun: 'Compiling to ES modules\n',
+      onSuccess: 'Compiled to ES modules with success\n',
+    },
+  },
+  TO_CMJS: {
+    command: 'babel modules -d cmjs --ignore __tests__',
+    moduleType: 'cmjs',
+    messages: {
+      onBeforeRun: 'Compiling to CMJS modules\n',
+      onSuccess: 'Compiled to CMJS modules with success\n',
+    },
+  },
+  TO_DEV_UMD: {
+    command: 'rollup -c -i modules/index.js -o umd/form-base.js',
+    moduleType: 'es',
+    env: 'development',
+    messages: {
+      onBeforeRun: 'Compiling to development UMD module\n',
+      onSuccess: 'Compiled to development UMD module with success\n',
+    },
+  },
+  TO_PROD_UMD: {
+    command: 'rollup -c -i modules/index.js -o umd/form-base.min.js',
+    moduleType: 'es',
+    env: 'production',
+    messages: {
+      onBeforeRun: 'Compiling to production UMD module\n',
+      onSuccess: 'Compiled to production UMD module with success\n',
+    },
+  },
+};
+
+
+module.exports =  INFO_TO_COMPILE;
