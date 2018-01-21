0fd2a36d0dc0a988ebdcce9a51953eebdb7bca1c
kirill <best.portfoleo@gmail.com>
2018-01-21 19:03:17 +0500

    Add formBaseSaga module

--- src/packages/form-base/modules/actions/formBaseActions.js
+++ src/packages/form-base/modules/actions/formBaseActions.js
@@ -1,2 +1,4 @@
 export const INIT_FORM = '@@formBase/INIT_FORM';
+
 export const SUBMIT_FORM = '@@formBase/SUBMIT_FORM';
+export const SUBMIT_FORM_WITH_SUCCESS = '@@formBase/SUBMIT_FORM_WITH_SUCCESS';
