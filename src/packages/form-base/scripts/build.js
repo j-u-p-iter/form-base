93e6b40ba4d4e11e6b41612cbcea1cda65ad7b27
kirill <best.portfoleo@gmail.com>
2018-01-31 15:45:42 +0500

    Some fixes

--- src/packages/form-base/scripts/build.js
+++ src/packages/form-base/scripts/build.js
@@ -1,7 +1,7 @@
 const createModulesBuilder = require('modules-builder');
+
 const INFO_TO_COMPILE = require('./compile-config');
 
+
 const buildModules = createModulesBuilder(INFO_TO_COMPILE);
-
-
-buildModules(INFO_TO_COMPILE);
+buildModules();
